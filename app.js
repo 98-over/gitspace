77b666a6a434ba63141e40b29325009883486e26
shichang_zhang <577002720@qq.com>
2020-11-16 22:35:50 +0800

    xxx

--- app.js
+++ app.js
@@ -83,8 +83,8 @@ var wsServer = new ws.Server({ server: httpsServer });
 wsServer.on('connection', function (wsConnect, request) {
   console.log("连接成功");
   wsConnect.on('message', function (message) {
-    var data = JSON.stringify(message);
-    console.log(message.op);
+    var data = JSON.parse(message);
+    console.log(data.op);
     if (data.op == 'ping') {
       var r = {
         type: '1'
