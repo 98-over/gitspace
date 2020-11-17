a973163c703ca9d0f7fc950779290de4681fb54e
shichang_zhang <577002720@qq.com>
2020-11-17 18:32:02 +0800

    xxx

--- routes/getUser.js
+++ routes/getUser.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   //var users = [];
-  var sql = 'SELECT DISTINCT * from user_info WHERE open_id in(SELECT DISTINCT receive_id FROM message WHERE meg_id like ? or ?)or open_id in(SELECT DISTINCT send_id FROM message WHERE meg_id like ? or ?) and open_id != ?';
+  var sql = 'SELECT DISTINCT * from user_info WHERE open_id != ? and open_id in(SELECT DISTINCT receive_id FROM message WHERE meg_id like ? or ?)or open_id != ? and open_id in(SELECT DISTINCT send_id FROM message WHERE meg_id like ? or ?)';
   //var sql = 'SELECT DISTINCT open_id,nick_name,avatarUrl from user_info,message where user_info.open_id=message.receive_id OR user_info.open_id=message.send_id and open_id=?';
   var connection = mysql.createConnection({
     host:'47.98.206.11',
@@ -16,7 +16,7 @@ router.get('/', function (req, res, next) {
     multipleStatements: true 
   });
   connection.connect();
-  connection.query(sql,['%'+req.query.openId,req.query.openId+'%','%'+req.query.openId,req.query.openId+'%',req.query.openId],function(err,results){
+  connection.query(sql,[req.query.openId,'%'+req.query.openId,req.query.openId+'%',req.query.openId,'%'+req.query.openId,req.query.openId+'%'],function(err,results){
     if(err) throw err;
     if(results[0] != null){
       res.json(results);
